7cb83775c5587eaae9ad93ada7a5f09bf0523bf5
Elie Magambo <eliemagambo@gmail.com>
2026-09-04 23:10:52 +0900

    Added training

--- src/preprocessing/preprocess.py
+++ src/preprocessing/preprocess.py
@@ -6,7 +6,9 @@ import os
 import json
 import re
 import random 
+import torch
 from pathlib import Path
+
 from typing import List, Dict, Any
 
 import numpy as np
@@ -19,6 +21,11 @@ def set_seed(seed: int):
     """
     random.seed(seed)
     np.random.seed(seed)
+    torch.manual_seed(seed)
+
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+
 
 def load_json_file(file_path: Path| str) -> List[Dict[str, Any]]:
     """
